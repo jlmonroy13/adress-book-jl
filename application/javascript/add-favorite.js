8ab7d5addc1366d3ec7c529c0cf02abec9bfe991
jlmonroy13 <jlmonroy13@gmail.com>
2016-04-04 18:02:23 -0500

    first

--- application/javascript/add-favorite.js
+++
@@ -1,6 +0,0 @@
-/* SDFSADNFKLUSADHFSDJKLFHBASDJKFBASDLUKVBNASDKLJVNBSDALVNL */
-
-function addFavorite(array, fjsnode, jsnode) {
-  renderHTML(array, jsnode);
-  renderHTMLfavorite(array, fjsnode);
-}
